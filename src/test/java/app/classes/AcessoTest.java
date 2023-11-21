36bf1917301f4d14189f08db9eece9af8eac500f
gaLonghi01 <gabriel.longhi02@gmail.com>
2023-11-21 18:48:42 -0300

    Começo classes acesso e veículo Ayuda

---
+++ src/test/java/app/classes/AcessoTest.java
@@ -0,0 +1,23 @@
+package app.classes;
+import classes.Acesso;
+import exceptions.EstacionamentoFechadoException;
+import exceptions.PeriodoInvalidoException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AcessoTest {
+    private Acesso periodo;
+    
+    @BeforeEach
+    public void setUp(){
+        periodo = new Acesso();
+        periodo.
+    }
+}
---
+++ src/test/java/app/classes/VeiculoTest.java
@@ -0,0 +1,12 @@
+package app.classes;
+import exceptions.DadosVeiculosIncompletosException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VeiculoTest {
+    
+}
