17567b8ca37ed615beb6cde6a5b180da93995d34
gaLonghi01 <gabriel.longhi02@gmail.com>
2023-11-22 23:12:18 -0300

    Atualização Veiculo.java ...

--- src/test/java/classes/VeiculoTest.java
+++ src/test/java/classes/VeiculoTest.java
@@ -1,4 +1,4 @@
-package app.classes;
+package classes;
 import exceptions.DadosVeiculosIncompletosException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,4 +9,23 @@ import org.junit.jupiter.api.Test;
 
 public class VeiculoTest {
     
+    @Test
+    public void VeiculoValido() throws DadosVeiculosIncompletosException {
+        Veiculo veiculo = new Veiculo();
+        veiculo.setMarca("Honda");
+        veiculo.setModelo("Civic");
+        veiculo.setNumeroPlaca("abc1234");
+
+        assertEquals("Honda", veiculo.getMarca());
+        assertEquals("Civic", veiculo.getModelo());
+        assertEquals("abc1234", veiculo.getNumeroPlaca());
+    }
+
+    @Test
+    public void naoChegaMarca() {
+        Veiculo veiculo = new Veiculo();
+        assertThrows(DadosVeiculosIncompletosException.class, () -> {
+            veiculo.setMarca("");
+        });
+    }
 }
